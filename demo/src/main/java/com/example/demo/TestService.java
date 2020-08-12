6151a9248aead02e1c7c88203c1dc1bf067e097b
2293736867 <2293736867@qq.com>
2020-08-12 22:16:52 +0800

    Hibernate Validator初始化

--- demo/src/main/java/com/example/demo/TestService.java
+++ demo/src/main/java/com/example/demo/TestService.java
@@ -1,19 +1,19 @@
 package com.example.demo;
 
 import com.example.demo.entity.User;
+import org.springframework.http.codec.EncoderHttpMessageWriter;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DataBinder;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
 
 @Service
 public class TestService {
     public String test(User user)
     {
-        if(StringUtils.isEmpty(user.getEmail()))
-            return "邮箱不能为空";
-        if(StringUtils.isEmpty(user.getPassword()))
-            return "密码不能为空";
-        if(StringUtils.isEmpty(user.getPhone()))
-            return "电话不能为空";
 //        持久化操作
         return "success";
     }
--- demo/src/main/java/com/example/demo/entity/User.java
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -1,10 +1,20 @@
 package com.example.demo.entity;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 public class User {
+    @NotEmpty(message = "密码不能为空")
+    @Length(min = 11,max = 11,message = "电话号码必须11位")
     private String phone;
+    @NotEmpty(message = "密码不能为空")
+    @Length(min = 6,max = 20,message = "密码必须为6-20位")
     private String password;
+    @NotEmpty(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
     private String email;
 }
