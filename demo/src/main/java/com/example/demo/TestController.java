aa60b645f248abb62fcbea68bc9a44a71cd0d19b
2293736867 <2293736867@qq.com>
2020-08-12 23:48:38 +0800

    分组

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -4,6 +4,8 @@ import com.example.demo.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.*;
@@ -15,18 +17,15 @@ import java.util.Set;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TestController {
     private final TestService service;
-    private final FailFastValidator<User> validator;
     @PostMapping("test")
-    public String test(@RequestBody User user, BindingResult bindingResult)
+    public String test(@RequestBody @Validated({User.GroupB.class}) User user, BindingResult bindingResult)
     {
-        Set<ConstraintViolation<User>> message = validator.validate(user);
-        message.forEach(t-> System.out.println(t.getMessage()));
-//        if(bindingResult.hasErrors())
-//        {
-//            bindingResult.getAllErrors().forEach(t->System.out.println(t.getDefaultMessage()));
-//            for(ObjectError error:bindingResult.getAllErrors())
-//                return error.getDefaultMessage();
-//        }
+        if(bindingResult.hasErrors())
+        {
+            bindingResult.getAllErrors().forEach(t->System.out.println(t.getDefaultMessage()));
+            for(ObjectError error:bindingResult.getAllErrors())
+                return error.getDefaultMessage();
+        }
         return service.test(user);
     }
 }
--- demo/src/main/java/com/example/demo/entity/User.java
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -9,7 +9,8 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class User {
     @NotEmpty(message = "密码不能为空")
-    @Length(min = 11,max = 11,message = "电话号码必须11位")
+    @Length(min = 11,max = 11,message = "电话号码必须11位",groups = {GroupA.class})
+    @Length(min = 12,max = 12,message = "电话号码必须12位",groups = {GroupB.class})
     private String phone;
     @NotEmpty(message = "密码不能为空")
     @Length(min = 6,max = 20,message = "密码必须为6-20位")
@@ -17,4 +18,7 @@ public class User {
     @NotEmpty(message = "邮箱不能为空")
     @Email(message = "邮箱格式不正确")
     private String email;
+
+    public interface GroupA{}
+    public interface GroupB{}
 }
