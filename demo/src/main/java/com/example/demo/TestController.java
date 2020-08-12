0d9ffb9e16105566d29897bc60acd9918b1855d2
2293736867 <2293736867@qq.com>
2020-08-12 22:35:48 +0800

    Hibernate Validator快速失败模式

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -2,11 +2,8 @@ package com.example.demo;
 
 import com.example.demo.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.HibernateValidator;
-import org.hibernate.validator.HibernateValidatorConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.*;
@@ -18,15 +15,18 @@ import java.util.Set;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TestController {
     private final TestService service;
+    private final FailFastValidator<User> validator;
     @PostMapping("test")
     public String test(@RequestBody User user, BindingResult bindingResult)
     {
-        if(bindingResult.hasErrors())
-        {
-            bindingResult.getAllErrors().forEach(t->System.out.println(t.getDefaultMessage()));
-            for(ObjectError error:bindingResult.getAllErrors())
-                return error.getDefaultMessage();
-        }
+        Set<ConstraintViolation<User>> message = validator.validate(user);
+        message.forEach(t-> System.out.println(t.getMessage()));
+//        if(bindingResult.hasErrors())
+//        {
+//            bindingResult.getAllErrors().forEach(t->System.out.println(t.getDefaultMessage()));
+//            for(ObjectError error:bindingResult.getAllErrors())
+//                return error.getDefaultMessage();
+//        }
         return service.test(user);
     }
 }
